9a440cd4b04050fee659d5f7edcdecc2fde71302
KiddaCLD <94736684+KiddaCLD@users.noreply.github.com>
2022-11-29 19:38:08 +0700

    Add files via upload

--- main.js
+++ main.js
@@ -1,16 +1,30 @@
 document.querySelector('#reg').onclick = Registory; //обработчик нажатия
 flag = 0; // если 1 то пользователь зарегестрирован а если 0 то нет
 
-if (flag == 0){
-	document.querySelector('.text').onload = unregister();
+if (getCookie("reg") == 1){
+	setinfo()
 }
 else{
-	document.querySelector('.label').onload = SetCountConnect(2);
+	unregister();
 }
 
 
 function setCookie(name, value, expire) {  
 	document.cookie = name + "=" + escape(value) + ((expire == null) ? "" : ("; expires=" + expire.toGMTString()))
+}
+
+function getCookie(Name) {   
+	var search = Name + "="
+	if (document.cookie.length > 0) {
+		offset = document.cookie.indexOf(search);
+		if (offset != -1) {
+			offset += search.length;
+			end = document.cookie.indexOf(";", offset);
+			if (end == -1)
+				end = document.cookie.length;
+			return unescape(document.cookie.substring(offset, end));
+		}
+	}
 }
 
 
@@ -19,14 +33,25 @@ function Registory() {
 	let expires = new Date()
 	expires.setTime(today.getTime() + 1000*60*60*24*365)
 	let name = document.querySelector(".regreg").value;
-	setCookie("reg",0, expires);
+	setCookie("reg", 1, expires);
 	setCookie("name", name, expires);
+	setCookie("c_c", 1, expires);
+	setCookie("lastCon", today , expires);
+
 }
 
 function unregister(){
 	document.querySelector("p#name").textContent = "Вы еще не зарегестрировались!";
 	document.querySelector("p#count").textContent = "количество посещений: 1";
 	document.querySelector("p#time").textContent = "Вы были на этой странице в последний раз: никогда ";	
+}
+
+
+function setinfo(){
+	console.log(getCookie("name"))
+	console.log(getCookie("reg"))
+	console.log(getCookie("c_c"))
+	console.log(getCookie("lastCon"))
 }
 
 function setName(name){
